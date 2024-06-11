35272ddd958094d38abbf5105b77ef2f81c3a46d
Joshua Granick <jgranick@users.noreply.github.com>
2024-06-10 22:07:38 -0700

    Add AL.EXT_float32 to playback 32-bit audio samples

--- src/lime/_internal/backend/native/NativeAudioSource.hx
+++ src/lime/_internal/backend/native/NativeAudioSource.hx
@@ -1,5 +1,6 @@
 package lime._internal.backend.native;
 
+import lime.media.openal.ext.EXT_float32;
 import haxe.Int64;
 import haxe.Timer;
 import lime.app.Application;
@@ -92,6 +93,10 @@ class NativeAudioSource
 			{
 				format = AL.FORMAT_MONO16;
 			}
+			else
+			{
+				format = EXT_float32.FORMAT_MONO_FLOAT32;
+			}
 		}
 		else if (parent.buffer.channels == 2)
 		{
@@ -102,6 +107,10 @@ class NativeAudioSource
 			else if (parent.buffer.bitsPerSample == 16)
 			{
 				format = AL.FORMAT_STEREO16;
+			}
+			else
+			{
+				format = EXT_float32.FORMAT_STEREO_FLOAT32;
 			}
 		}
 
