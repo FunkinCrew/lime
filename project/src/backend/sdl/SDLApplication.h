35120d076a6d0a17ed66485863473a7ac17b3562
MAJigsaw77 <77043862+MAJigsaw77@users.noreply.github.com>
2025-06-14 12:44:14 +0300

    Fixed `android` completly.

--- project/src/backend/sdl/SDLApplication.h
+++ project/src/backend/sdl/SDLApplication.h
@@ -47,7 +47,9 @@ namespace lime {
 			void ProcessJoystickEvent (SDL_Event* event);
 			void ProcessKeyEvent (SDL_Event* event);
 			void ProcessMouseEvent (SDL_Event* event);
+			#if defined(ANDROID) || defined (IPHONE)
 			void ProcessSensorEvent (SDL_Event* event);
+			#endif
 			void ProcessTextEvent (SDL_Event* event);
 			void ProcessTouchEvent (SDL_Event* event);
 			void ProcessWindowEvent (SDL_Event* event);
