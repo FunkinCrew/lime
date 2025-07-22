1d1b85330bee4d7ec5f35b4fce0cac107b1833b4
Karim Akra <karimakra512@gmail.com>
2025-07-23 00:40:21 +0300

    Made the AudioSource actually check if the audio finished playing rather than a stupid timer

--- src/lime/_internal/backend/native/NativeAudioSource.hx
+++ src/lime/_internal/backend/native/NativeAudioSource.hx
@@ -2,6 +2,7 @@ package lime._internal.backend.native;
 
 import haxe.Int64;
 import haxe.Timer;
+import lime.app.Application;
 import lime.math.Vector4;
 import lime.media.openal.AL;
 import lime.media.openal.ALBuffer;
@@ -58,6 +59,14 @@ class NativeAudioSource
 	{
 		if (handle != null)
 		{
+			if (Application.current != null && !stream)
+			{
+				if (Application.current.onUpdate.has(checkPlay))
+				{
+					// trace('[AUDIO] removed play check event!');
+					Application.current.onUpdate.remove(checkPlay);
+				}
+			}
 			stop();
 			AL.sourcei(handle, AL.BUFFER, null);
 			AL.deleteSource(handle);
@@ -161,6 +170,15 @@ class NativeAudioSource
 		#end
 
 		samples = Std.int((dataLength * 8.0) / (parent.buffer.channels * parent.buffer.bitsPerSample));
+
+		if (Application.current != null && !stream)
+		{
+			if (!Application.current.onUpdate.has(checkPlay))
+			{
+					// trace('[AUDIO] added play check event!');
+				Application.current.onUpdate.add(checkPlay);
+			}
+		}
 	}
 
 	public function play():Void
@@ -384,6 +402,32 @@ class NativeAudioSource
 		parent.onComplete.dispatch();
 	}
 
+	private function checkPlay(delta:Int):Void
+	{
+		final finished:Bool = AL.getSourcei(handle, AL.SOURCE_STATE) != AL.PLAYING;
+
+		if (!finished) return;
+		if (loops > 0)
+		{
+			playing = false;
+			loops--;
+			setCurrentTime(0);
+			play();
+			return;
+		}
+		else
+		{
+			if (!completed)	stop();
+		}
+
+		if (!completed)
+		{
+			// trace('[AUDIO] audio finished playing!');
+			parent.onComplete.dispatch();
+		}
+		completed = true;
+	}
+
 	// Get & Set Methods
 	public function getCurrentTime():Int
 	{
@@ -468,8 +512,8 @@ class NativeAudioSource
 			if (timeRemaining > 0)
 			{
 				completed = false;
-				timer = new Timer(timeRemaining);
-				timer.run = timer_onRun;
+				// timer = new Timer(timeRemaining);
+				// timer.run = timer_onRun;
 			}
 			else
 			{
@@ -522,13 +566,13 @@ class NativeAudioSource
 				timer.stop();
 			}
 
-			var timeRemaining = Std.int((value - getCurrentTime()) / getPitch());
+			// var timeRemaining = Std.int((value - getCurrentTime()) / getPitch());
 
-			if (timeRemaining > 0)
-			{
-				timer = new Timer(timeRemaining);
-				timer.run = timer_onRun;
-			}
+			// if (timeRemaining > 0)
+			// {
+			// 	timer = new Timer(timeRemaining);
+			// 	timer.run = timer_onRun;
+			// }
 		}
 
 		return length = value;
@@ -565,13 +609,13 @@ class NativeAudioSource
 				timer.stop();
 			}
 
-			var timeRemaining = Std.int((getLength() - getCurrentTime()) / value);
+			// var timeRemaining = Std.int((getLength() - getCurrentTime()) / value);
 
-			if (timeRemaining > 0)
-			{
-				timer = new Timer(timeRemaining);
-				timer.run = timer_onRun;
-			}
+			// if (timeRemaining > 0)
+			// {
+			// 	timer = new Timer(timeRemaining);
+			// 	timer.run = timer_onRun;
+			// }
 		}
 
 		if (handle != null)
