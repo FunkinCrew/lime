325bc84f1117762a27166e8314ad911b4fa12ecb
Ralty <78720179+Raltyro@users.noreply.github.com>
2026-05-11 10:02:27 +0700

    WIP Audio Decoders for easily supporting more formats later

---
+++ project/include/media/decoders/WaveDecoder.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <media/AudioDecoder.h>
+
+
+namespace lime {
+
+
+	class WaveDecoder : public AudioDecoder {
+
+
+		public:
+
+			~WaveDecoder ();
+
+			bool Open (Resource* resource) override;
+			size_t Decode (void* ptr, size_t frames, int byteDepth) override;
+			bool Rewind () override;
+			bool Seek (int64_t frame) override;
+			bool CanSeek () override;
+			int64_t Tell () override;
+			int64_t Total () override;
+
+
+		private:
+
+			void* handle;
+
+
+	};
+
+
+}
