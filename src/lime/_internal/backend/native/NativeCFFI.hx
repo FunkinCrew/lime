ef8945bc11e76191d811e51bfaeedbda8979536f
MAJigsaw77 <mihaialexandru427@yahoo.com>
2026-08-03 22:54:30 +0300

    Comment the bindings

--- src/lime/_internal/backend/native/NativeCFFI.hx
+++ src/lime/_internal/backend/native/NativeCFFI.hx
@@ -25,6 +25,7 @@ typedef CFFIPointer = Dynamic;
 // @formatter:off
 class NativeCFFI
 {
+	// Lime bindings
 	#if (lime_cffi && !macro)
 	#if cpp
 	private static var lime_application_create = new cpp.Callable<Void->cpp.Object>(cpp.Prime._loadPrime("lime", "lime_application_create", "o", false));
@@ -385,8 +386,7 @@ class NativeCFFI
 	#end
 	#end
 
-
-
+	// JNI bindings
 	#if (lime_cffi && !macro && android)
 	#if cpp
 	private static var lime_jni_call_member = new cpp.Callable<cpp.Object->cpp.Object->cpp.Object->cpp.Object>(cpp.Prime._loadPrime("lime", "lime_jni_call_member", "oooo", false));
@@ -402,8 +402,7 @@ class NativeCFFI
 	#end
 	#end
 
-
-
+	// OpenAL bindings
 	#if (lime_cffi && !macro && lime_openal)
 	#if cpp
 	private static var lime_al_buffer_data = new cpp.Callable<cpp.Object->Int->cpp.Object->Int->Int->cpp.Void>(cpp.Prime._loadPrime("lime", "lime_al_buffer_data", "oioiiv", false));
@@ -658,8 +657,7 @@ class NativeCFFI
 	#end
 	#end
 
-
-
+	// Cairo bindings
 	#if (lime_cffi && !macro && lime_cairo)
 	#if cpp
 	private static var lime_cairo_arc = new cpp.Callable<cpp.Object->Float->Float->Float->Float->Float->cpp.Void>(cpp.Prime._loadPrime("lime", "lime_cairo_arc", "odddddv", false));
@@ -898,8 +896,7 @@ class NativeCFFI
 	#end
 	#end
 
-
-
+	// OpenGL bindings
 	#if (lime_cffi && !macro && (lime_opengl || lime_opengles))
 	#if cpp
 	private static var lime_gl_active_texture = new cpp.Callable<Int->cpp.Void>(cpp.Prime._loadPrime("lime", "lime_gl_active_texture", "iv", false));
@@ -1446,8 +1443,7 @@ class NativeCFFI
 	#end
 	#end
 
-
-
+	// Hurfbuzz bindings
 	#if (lime_cffi && !macro && lime_harfbuzz)
 	#if cpp
 	private static var lime_hb_blob_create = new cpp.Callable<lime.utils.DataPointer->Int->Int->cpp.Object>(cpp.Prime._loadPrime("lime", "lime_hb_blob_create", "diio", false));
