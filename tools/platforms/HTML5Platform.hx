31f17da30e9bd0f5c9fba5ce25776e82602e92e2
MAJigsaw77 <mihaialexandru427@yahoo.com>
2026-08-04 12:54:13 +0300

    Add `createDefaultProject` to `PlatformTarget`

--- tools/platforms/HTML5Platform.hx
+++ tools/platforms/HTML5Platform.hx
@@ -33,64 +33,11 @@ class HTML5Platform extends PlatformTarget
 	{
 		super(command, _project, targetFlags);
 
-		var defaults = new HXProject();
-
-		defaults.meta =
-			{
-				title: "MyApplication",
-				description: "",
-				packageName: "com.example.myapp",
-				version: "1.0.0",
-				company: "",
-				companyUrl: "",
-				buildNumber: null,
-				companyId: ""
-			};
-
-		defaults.app =
-			{
-				main: "Main",
-				file: "MyApplication",
-				path: "bin",
-				preloader: "",
-				url: "",
-				init: null
-			};
-
-		defaults.window =
-			{
-				width: 0,
-				height: 0,
-				parameters: "{}",
-				background: 0xFFFFFF,
-				fps: 60,
-				hardware: true,
-				display: 0,
-				resizable: true,
-				transparent: false,
-				borderless: false,
-				orientation: Orientation.AUTO,
-				vsync: false,
-				fullscreen: false,
-				allowHighDPI: true,
-				alwaysOnTop: false,
-				antialiasing: 0,
-				allowShaders: true,
-				requireShaders: false,
-				depthBuffer: true,
-				stencilBuffer: true,
-				colorDepth: 32,
-				maximized: false,
-				minimized: false,
-				hidden: false,
-				title: ""
-			};
-
-		for (i in 1...project.windows.length)
-		{
-			defaults.windows.push(defaults.window);
-		}
-
+		var defaults:HXProject = createDefaultProject();
+		defaults.window.width = 0;
+		defaults.window.height = 0;
+		defaults.window.allowHighDPI = true;
+		defaults.window.requireShaders = true;
 		defaults.merge(project);
 
 		project = defaults;
